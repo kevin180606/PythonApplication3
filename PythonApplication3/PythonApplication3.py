a6fe88cfa3ab8e6d9b07b8a61e088ebb7f63e42d
kevin180606 <39985754+kevin180606@users.noreply.github.com>
2019-07-09 15:29:36 +0800

    添加项目文件。

---
+++ PythonApplication3/PythonApplication3.py
@@ -0,0 +1,15 @@
+from math import cos,radians
+
+# Create a string with spaces proportional to a cosine of x in degrees 
+def make_dot_string(x):
+    rad =radians(x)
+    numspaces =int(20*cos(radians(x))+20)
+    st =' '*numspaces+'o'
+    return st
+
+def main():
+    for i in range(0,1800,12):
+        s=make_dot_string(i)
+        print(s)
+
+main()
